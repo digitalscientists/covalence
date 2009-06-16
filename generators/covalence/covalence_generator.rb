04cfe1fd5d074f2c03edbb610242318a38fbe677
matt.mongeau@digitalscientists.com <matt.mongeau@digitalscientists.com>
2009-06-16 10:44:07 -0400

    Now may install individual parts of covalence instead

--- generators/covalence/covalence_generator.rb
+++ generators/covalence/covalence_generator.rb
@@ -1,10 +1,9 @@
 class CovalenceGenerator < Rails::Generator::Base
- 
   def manifest
     record do |m|
-      m.migration_template "migrations/generate_covalence.rb",
-                           'db/migrate',
-                           :migration_file_name => "generate_covalence"
-    end
+      type = args[0] || nil
+      m.migration_template "migrations/generate_covalence_groups.rb", 'db/migrate', :migration_file_name => "generate_covalence_groups" unless type == 'notifications'
+      m.migration_template "migrations/generate_covalence_notifications.rb", 'db/migrate', :migration_file_name => "generate_covalence_notifications" unless type == 'groups'      
+    end    
   end 
 end
--- generators/covalence/templates/migrations/generate_covalence_groups.rb
+++ generators/covalence/templates/migrations/generate_covalence_groups.rb
@@ -1,18 +1,5 @@
-class GenerateCovalence < ActiveRecord::Migration
+class GenerateCovalenceGroups < ActiveRecord::Migration
   def self.up
-    create_table "covalence_notifications", :force => true do |t|
-      t.string   "consumer_type"
-      t.integer  "consumer_id"
-      t.string   "producer_type"
-      t.integer  "producer_id"
-      t.string   "type"
-      t.string   "state", :default => 'new'
-      t.text     "message"
-      t.datetime "created_at"
-      t.datetime "updated_at"
-      t.datetime "published_at", :default => Time.now
-    end
-
     create_table "covalence_relationships", :force => true do |t|
       t.string   "parent_type"
       t.integer  "parent_id"
@@ -32,11 +19,6 @@ class GenerateCovalence < ActiveRecord::Migration
       t.string  "groupable_type"
     end
     
-    
-    add_index :covalence_notifications, [:producer_id, :producer_type]
-    add_index :covalence_notifications, [:consumer_id, :consumer_type]
-    add_index :covalence_notifications, :type
-    add_index :covalence_notifications, :state
     add_index :covalence_relationships, :type
     add_index :covalence_relationships, :state
     add_index :covalence_relationships, [:parent_id, :parent_type]
@@ -47,7 +29,6 @@ class GenerateCovalence < ActiveRecord::Migration
   end
   
   def self.down
-    drop_table "covalence_notifications"
     drop_table "covalence_relationships"
     drop_table "covalence_assets"
   end
