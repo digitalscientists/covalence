1166111ae1c413616618b4672f5896fdb84236df
matt.mongeau@digitalscientists.com <matt.mongeau@digitalscientists.com>
2009-06-16 11:00:56 -0400

    Notifications doesn't need tables anymore.

--- generators/covalence/covalence_generator.rb
+++ generators/covalence/covalence_generator.rb
@@ -3,7 +3,7 @@ class CovalenceGenerator < Rails::Generator::Base
     record do |m|
       type = args[0] || nil
       m.migration_template "migrations/generate_covalence_groups.rb", 'db/migrate', :migration_file_name => "generate_covalence_groups" unless type == 'notifications'
-      m.migration_template "migrations/generate_covalence_notifications.rb", 'db/migrate', :migration_file_name => "generate_covalence_notifications" unless type == 'groups'      
+      # m.migration_template "migrations/generate_covalence_notifications.rb", 'db/migrate', :migration_file_name => "generate_covalence_notifications" unless type == 'groups'      
     end    
   end 
 end
