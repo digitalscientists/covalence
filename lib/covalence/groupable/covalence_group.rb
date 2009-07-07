9b95e96091d122b0108d200f978b6df274bffee4
matt.mongeau@digitalscientists.com <matt.mongeau@digitalscientists.com>
2009-07-07 15:46:29 -0400

    Added options for members/groups. Added dependent destroy

--- lib/covalence/groupable/covalence_group.rb
+++ lib/covalence/groupable/covalence_group.rb
@@ -3,6 +3,13 @@ module Covalence
   module Group
     def self.included(model)
       model.extend(ClassMethods)
+      model.class_eval do
+        def self.covalence_options
+          @covalence_options ||= {
+            :membership_class => 'Membership'
+          }
+        end
+      end
     end
     
     module ClassMethods
@@ -17,9 +24,10 @@ module Covalence
       end
       
       def has_members *members
-        has_many :covalence_memberships, :as => :parent 
+        covalence_options.merge!(members.pop) if members.last.is_a? Hash
+        has_many :memberships, :as => :parent, :class_name => covalence_options[:membership_class], :dependent => :destroy 
         members.each do |member|
-          has_many member, :through => :covalence_memberships, :source => 'child', :source_type => member.to_s.classify do
+          has_many member, :through => :memberships, :source => 'child', :source_type => member.to_s.classify do
             
             def <<(*args)
               args.each do |arg|
@@ -28,15 +36,15 @@ module Covalence
             end
             
             def remove(member)
-              @owner.covalence_memberships.find_by_child_type_and_child_id(member.class.name, member.id).destroy
+              @owner.memberships.find_by_child_type_and_child_id(member.class.name, member.id).destroy
               @target.delete(member)
             end
             
             def create_with_role(member, role)
               if role != nil
-                @owner.covalence_memberships.create(:child => member, :status => role.to_s)
+                @owner.memberships.create(:child => member, :status => role.to_s)
               elsif @owner.class.respond_to?(:default_role)
-                @owner.covalence_memberships.create(:child => member, :status => @owner.class.default_role.to_s)
+                @owner.memberships.create(:child => member, :status => @owner.class.default_role.to_s)
               end
             end
             
@@ -44,6 +52,7 @@ module Covalence
             def join(member, role = nil)
               create_with_role(member, role)
             end
+           
           end
         end
       end
@@ -69,15 +78,11 @@ module Covalence
     end
     
     def members
-      self.covalence_memberships.map(&:child)
-    end
-    
-    def memberships
-      self.covalence_memberships
+      self.memberships.map(&:child)
     end
     
     def with_role(role)
-      self.covalence_memberships.all(:conditions => ['status = ?', role.to_s]).map(&:child)
+      self.memberships.all(:conditions => ['status = ?', role.to_s]).map(&:child)
     end
   end
 end
--- lib/covalence/groupable/covalence_member.rb
+++ lib/covalence/groupable/covalence_member.rb
@@ -3,25 +3,34 @@ module Covalence
   module Member
     def self.included(model)
       model.extend(ClassMethods)
+      model.class_eval do
+        def self.covalence_options
+          @covalence_options ||= {
+            :membership_class => 'Membership'
+          }
+        end
+      end
     end
-    
+      
     module ClassMethods
+      
       def is_member_of *groups
-        has_many :covalence_memberships, :as => :child
+        covalence_options.merge!(groups.pop) if groups.last.is_a? Hash
+        has_many :memberships, :as => :child, :class_name => covalence_options[:membership_class]
         groups.each do |group|
-          has_many group, :through => :covalence_memberships, :source => 'parent', :source_type => group.to_s.classify, :conditions => ['state is null or state != ?', 'pending']
+          has_many group, :through => :memberships, :source => 'parent', :source_type => group.to_s.classify, :conditions => ['state is null or state != ?', 'pending']
         end
       end
     end 
     
     def role_in(group)
-      membership = self.covalence_memberships.find_by_parent_id_and_parent_type(group.id, group.class.name)
-      return membership ? membership.role : false
+      membership = self.memberships.find_by_parent_id_and_parent_type(group.id, group.class.name)
+      return membership ? membership.status : false
     end
     
     def member_in?(group)
       # if you don't know the !! is for, you probably shouldn't be here
-      !!self.covalence_memberships.find_by_parent_id_and_parent_type(group.id, group.class.name)
+      !!self.memberships.find_by_parent_id_and_parent_type(group.id, group.class.name)
     end
     
     def method_missing method, *args, &block
@@ -29,13 +38,13 @@ module Covalence
         group = args[0]
         role = method.to_s.match(/^is_(.*)_of\?$/).captures[0].upcase.to_sym
         if group.has_role?(role)
-          return self.role_in(group) == role
+          return self.role_in(group).to_sym == role
         end
       elsif method.to_s =~ /^is_.*\?$/
         group = args[0]
         role = method.to_s.match(/^is_(.*)\?$/).captures[0].upcase.to_sym
         if group.has_role?(role)
-          return self.role_in(group) == role
+          return self.role_in(group).to_sym == role
         end
       elsif method.to_s =~ /^is_.*_of$/
         role = method.to_s.match(/^is_(.*)_of$/).captures[0].upcase.to_sym
