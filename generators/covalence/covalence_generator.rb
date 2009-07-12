6bb9a5d63e1bef3e02bebaf10d34848a577d8781
Matthew Mongeau <halogenandtoast@matthew-mongeaus-imac.local>
2009-07-12 15:14:12 -0400

    Added Generators

--- generators/covalence/covalence_generator.rb
+++
@@ -1,9 +0,0 @@
-class CovalenceGenerator < Rails::Generator::Base
-  def manifest
-    record do |m|
-      type = args[0] || nil
-      m.migration_template "migrations/generate_covalence_groups.rb", 'db/migrate', :migration_file_name => "generate_covalence_groups" unless type == 'notifications'
-      # m.migration_template "migrations/generate_covalence_notifications.rb", 'db/migrate', :migration_file_name => "generate_covalence_notifications" unless type == 'groups'      
-    end    
-  end 
-end
